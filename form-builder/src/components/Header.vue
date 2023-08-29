32b2ff7bfe13c557ce0db9aa92571078bd1fd916
muhammad salman <111515109+Muhammadsalman53@users.noreply.github.com>
2023-08-29 15:29:12 +0500

    improved

--- form-builder/src/components/Header.vue
+++ form-builder/src/components/Header.vue
@@ -1,61 +1,81 @@
 <template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    
-      <div class="container">
-    
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-    
-          <span class="navbar-toggler-icon"></span>
-    
-        </button>
-    
-        <a class="navbar-brand" href="#"><img src="../assets/images/form builder.webp" style="width: 70px; height: 70px;"></a>
-    
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-    
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-    
-            <li class="nav-item">
-    
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-    
-            </li>
-    
-            <!-- <li class="nav-item">
-    
-              <a class="nav-link" href="#">Link</a>
-    
-            </li>
-    
-            <li class="nav-item">
-    
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-    
-            </li> -->
-    
-          </ul>
-    
-          <form class="d-flex">
-    
-           
-    
-            <img style="width: 50px; height:50px; border-radius: 50%;" src="../assets/images/men.PNG" alt="">
-    
-          </form>
-    
-        </div>
-    
+    <div class="container">
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03"
+        aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+
+        <span class="navbar-toggler-icon"></span>
+
+      </button>
+
+      <a class="navbar-brand" href="#"><img src="../assets/images/form builder.webp"
+          style="width: 70px; height: 70px;"></a>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+
+        <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
+
+          <li class="nav-item dropdown">
+
+            <a class="nav-link dropdown-toggle text-black" href="#" id="navbarDropdown" role="button"
+              data-mdb-toggle="dropdown" aria-expanded="false">
+
+              <!-- <i class="bi bi-person mx-1"></i> Profile -->
+
+              <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle img-fluid"
+                style="width: 40px" alt="Avatar" />
+
+            </a>
+
+            <!-- Dropdown menu -->
+
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+
+              <li>
+
+                <a class="dropdown-item" href="#">My account</a>
+
+              </li>
+
+              <li>
+
+                <a class="dropdown-item" style="cursor: pointer" @click="logout">Log out</a>
+
+              </li>
+
+            </ul>
+
+          </li>
+
+        </ul>
+
+
+
       </div>
+
+    </div>
+
+  </nav>
+</template>
     
-    </nav>
-    
-    </template>
     
     
-    
-    <script>
-    export default{
-        name: 'Header'
-    }
-    </script>
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style>
+#navbarTogglerDemo03 {
+  justify-content: space-between;
+}
+
+div#navbarTogglerDemo03 ul>li:hover>ul {
+
+  display: block;
+
+}
+</style>
