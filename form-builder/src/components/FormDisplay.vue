40ef46867be104b7c671329ede2401f4fd88f4d2
muhammad salman <111515109+Muhammadsalman53@users.noreply.github.com>
2023-09-01 14:43:27 +0500

    display form

--- form-builder/src/components/FormDisplay.vue
+++ form-builder/src/components/FormDisplay.vue
@@ -1,26 +1,91 @@
 <template>
-    <div>
-      <h2>HTML Forms</h2>
-      <!-- Render the HTML form elements here based on the JSON data array -->
-      <div v-for="(formData, index) in formDataArray" :key="index">
-        <form>
-          <div v-for="(field, fieldName) in formData" :key="fieldName">
-            <label :for="fieldName">{{ field.label }}</label>
-            <input :type="field.type" :id="fieldName" :name="fieldName" v-model="field.value" />
-            <!-- Debugging: Display the JSON data for this field -->
-            <pre>{{ field }}</pre>
-          </div>
-        </form>
-      </div>
+  <div>
+    <h2>HTML Forms</h2>
+    <!-- Render the HTML form elements here based on the fetched JSON data -->
+    <div v-if="formDataArray.length === 0">
+      <p>Loading data...</p>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "FormDisplay",
-    props: {
-      formDataArray: Array, // Array of JSON data received from the URLs
-    },
-  };
-  </script>
-  
+    <div v-else>
+      <form>
+        <div v-for="(formData, index) in formDataArray" :key="index">
+          <!-- Your dynamic form rendering code here -->
+          <div v-if="formData.type === 'button'">
+            <button
+              :class="formData.className"
+              :name="formData.name"
+              :style="formData.style"
+            >
+              {{ formData.label }}
+            </button>
+          </div>
+          <div v-else-if="formData.type === 'header'">
+            <h1>{{ formData.label }}</h1>
+          </div>
+          <div v-else-if="formData.type === 'radio-group'">
+            <label>{{ formData.label }}</label>
+            <div v-for="(radioOption, optionIndex) in formData.values" :key="optionIndex">
+              <input
+                type="radio"
+                :id="radioOption.value"
+                :name="formData.name"
+                :value="radioOption.value"
+                v-model="radioOption.selected"
+              />
+              <label :for="radioOption.value">{{ radioOption.label }}</label>
+            </div>
+          </div>
+          <div v-else-if="formData.type === 'select'">
+            <label>{{ formData.label }}</label>
+            <select
+              :class="formData.className"
+              :name="formData.name"
+              :multiple="formData.multiple"
+              v-model="formData.values"
+            >
+              <option
+                v-for="(selectOption, optionIndex) in formData.values"
+                :key="optionIndex"
+                :value="selectOption.value"
+              >
+                {{ selectOption.label }}
+              </option>
+            </select>
+          </div>
+          <!-- You can add more conditions for other form field types here -->
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formDataArray: [],
+    };
+  },
+  mounted() {
+    // Replace "your-auth-token" with your actual authorization token
+    const token = "your-auth-token";
+
+    fetch('https://5de9-182-176-157-31.ngrok-free.app/api/display-json/fmYU6XoZuM6tDFF434NilOeAL4bQTDSF', {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "ngrok-skip-browser-warning": "afsd",
+        "Authorization": `Bearer ${token}`,
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Store the fetched JSON data in formDataArray
+        this.formDataArray = data.data;
+        console.log(this.formDataArray);
+      })
+      .catch(error => {
+        console.error('Error fetching JSON data:', error);
+      });
+  },
+};
+</script>
